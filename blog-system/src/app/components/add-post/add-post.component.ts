7fc97fadbe026cb79a3dff6504546b2b111049c0
Kaveesha_Walalawela <kaveesha@hsenidlanka.com>
2023-05-04 12:26:07 +0530

    Add dummy data and Two warning messages

--- blog-system/src/app/components/add-post/add-post.component.ts
+++ blog-system/src/app/components/add-post/add-post.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 // import { FormControl, FormGroup, Validators } from '@angular/core';
+import { PostService } from 'src/app/service/post.service';
 
 @Component({
   selector: 'app-add-post',
@@ -8,7 +9,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./add-post.component.css']
 })
 export class AddPostComponent implements OnInit {
-  constructor(){
+  constructor(private postService:PostService){
 
   }
   addPost = new FormGroup({
@@ -19,7 +20,13 @@ export class AddPostComponent implements OnInit {
 });
 onSubmit(){
   console.log(this.addPost.value);
-}
+  this.postService.createPost(this.addPost.value).subscribe((data)=>{
+    alert("Blog Post Successful !");
+    this.addPost.reset()
+  },(error)=>{
+    console.log("Error Occured !")
+  })
+  }
 
 ngOnInit(): void {
 
