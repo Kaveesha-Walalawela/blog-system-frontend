6b478efa6839c1b86668da679f1b8ceeef80d604
Kaveesha_Walalawela <kaveesha@hsenidlanka.com>
2023-05-22 10:04:24 +0530

    home dropdown acess control

--- blog-system/src/app/components/home/home.component.ts
+++ blog-system/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from 'src/app/service/post.service';
+import { UserService } from 'src/app/service/user.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,26 +12,40 @@ export class HomeComponent implements OnInit {
   posts: any;
   term: string = '';
   filteredPosts: any;
-  
-  constructor(private postService: PostService, private router: Router) {}
+  loggedInUser: any;
+
+  constructor(
+    private postService: PostService,
+    private userService: UserService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.postService.getAllPosts().subscribe(
       (data) => {
-        this.posts = data.filter((post: any) => post.status !== 'DRAFT' && post.status !== 'PENDING' && post.status !== 'REJECTED'); // Filter out draft posts
+        this.posts = data.filter(
+          (post: any) =>
+            post.status !== 'DRAFT' &&
+            post.status !== 'PENDING' &&
+            post.status !== 'REJECTED'
+        ); // Filter out draft posts
         this.filteredPosts = this.posts;
       },
       (error) => {
         console.warn('Some error occurred!');
       }
     );
+
+    this.loggedInUser = this.userService.getLoggedInUser();
   }
 
   deletePost(post: any) {
     this.postService.deletePost(post.id).subscribe(
       () => {
         this.posts = this.posts.filter((p: any) => p.id !== post.id);
-        this.filteredPosts = this.filteredPosts.filter((p: any) => p.id !== post.id);
+        this.filteredPosts = this.filteredPosts.filter(
+          (p: any) => p.id !== post.id
+        );
       },
       (error) => {
         console.warn('Some error occurred while deleting the post!');
@@ -52,14 +67,13 @@ export class HomeComponent implements OnInit {
     }, 3000); // Remove the message after 3 seconds
   }
 
+  isUserPost(post: any): boolean {
+    return post.username === this.loggedInUser?.username;
+  }
+
   filterPosts() {
     this.filteredPosts = this.posts.filter((post: any) =>
-post.title.toLowerCase().includes(this.term.toLowerCase())
-);
+      post.title.toLowerCase().includes(this.term.toLowerCase())
+    );
+  }
 }
-}
-
-
-
-
-//
